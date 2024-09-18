569b825f39ecac86d7e9550f6ffb2aac8717737e
Delsa Marasha <deemarasha77@gmail.com>
2024-09-18 05:21:30 +0200

    Refactor homepage styling and add logo

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../HomePage.css';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to AfriMind</h1>
-      <p>Your mental wellness companion for African women.</p>
-      <Link to="/register">Get Started</Link>
-      <br />
-      <Link to="/login">Login</Link>
+    <div className="home-page">
+      <header className="home-header">
+        <img src={process.env.PUBLIC_URL + '../../public/AFRIMIND_LOGO.png'} alt="Afrimind Logo" style={{ width: '200px' }} />
+        <h1>Welcome to AfriMind</h1>
+        <p>Empowering Women. Rooted in Africa</p>
+      </header>
+      
+      <div className="home-links">
+        <Link to="/register" className="btn-link">Get Started</Link>
+        <br />
+        <Link to="/login" className="btn-link">Login</Link>
+      </div>
+
+      <footer className="home-footer">
+        <p>&copy; 2024 AfriMind. All Rights Reserved.</p>
+      </footer>
     </div>
   );
 };
